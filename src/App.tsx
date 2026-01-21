385e467c6c25294a7a8ee20260327d3c83029cba
meteorelevrai <edouardbautz@gmail.com>
2026-01-21 03:44:56 +0100

    MàJ

--- src/App.tsx
+++ src/App.tsx
@@ -11,18 +11,20 @@ function App() {
   }
   return (
     <div className='bg-transparent'>
-      <div className='flex flex-col h-screen margin-5 gap-3 p-3'>
-        <div className='rounded-xl border-2 border-white w-full h-9/10 text-white justify-center'>
+      <div className='flex flex-col h-screen gap-3 p-3'>
+        <div className='bg-stone-800 border-1 border-stone-600 rounded-4xl w-full h-9/10 text-white justify-center p-10'>
           Hello World ! (Modèle actif : {activeModel})
         </div>
-        <div className='flex flex-col rounded-xl border-2 border-white w-full h-1/10 text-white justify-center'>
-          <input type="text" />
-          <button className='bg-white text-black w-1/10 rounded-sm' onClick={Menu}>{activeModel}</button>
+        <div className='flex self-center rounded-full text-white bg-stone-800 border-1 border-stone-600 p-4 gap-5 w-5/10'>
+          <input type="text" className='flex-1 outline-none text-lg ml-5' placeholder={`Demander à ${activeModel}`}/>
+          <div className='w-px h-full bg-stone-600'></div>
+          <button className='bg-[#ffffff40] text-white rounded-full cursor-pointer pl-10 pr-10 text-base border-1 border-[#ffffff80]' onClick={Menu}>{activeModel}</button>
           {isOpen && (
             <Popup 
               closeMenu={() => setIsOpen(false)}
               changeModel={(modelName) => setModel(modelName)}/>
           )}
+          <button className='bg-[#ffffff40] text-white rounded-full cursor-pointertext-base border-1 border-[#ffffff80] w-15 h-15'>➤</button>
         </div>
       </div>
     </div>
