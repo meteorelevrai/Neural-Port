c6c1d61efaccd26d23577729bdedabcc0477214a
Edouard Bautz <edouardbautz@gmail.com>
2026-01-22 13:46:09 +0100

    Add duplicate message display in Msg component

--- src/components/Msg.tsx
+++ src/components/Msg.tsx
@@ -2,6 +2,7 @@ function Msg({msg}: {msg: string}) {
     return (
         <div className="flex w-max max-w-full h-max bg-[#ffffff10] rounded-tr-xl rounded-br-xl rounded-bl-xl p-5 items-start break-all">
             <span>{msg}</span>
+            <span>{msg}</span>
         </div>
     )
 }
