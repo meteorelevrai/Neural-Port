1607b055e3ff29b2160051eb70791be2a4cb5b07
Edouard Bautz <edouardbautz@gmail.com>
2026-01-22 13:42:36 +0100

    Add Msg component and integrate message display in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,47 @@
 import { useState } from 'react'
 import { Models } from './models'
 import Popup from './components/Popup'
+import Msg from './components/Msg';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [activeModel, setModel] = useState(Models[0]);
+  const [inputValue, setInputValue] = useState("");
 
-  const Menu = () => {
+  const handleMenu = () => {
     setIsOpen(!isOpen);
   }
+
+  function textareaSize(event: React.ChangeEvent<HTMLTextAreaElement>) {
+    setInputValue(event.target.value)
+    const textarea = event.target;
+    textarea.style.height = 'auto'
+    textarea.style.height = textarea.scrollHeight + 'px'
+  } 
 
   return (
     <div className='bg-stone-900 h-screen w-full text-white'>
       
       <div className='flex flex-col h-full gap-4 p-4'>
-        <div className='bg-stone-800 border border-stone-600 rounded-3xl w-full flex-1 flex justify-center items-center p-10 overflow-hidden'>
-          Faire div messages
+        <div className='flex flex-col items-center flex-1'>
+          <div className='flex flex-col gap-5 bg-stone-800 border border-stone-600 rounded-3xl w-1/2 h-full p-10 overflow-y-scroll'>
+            <Msg msg={`Ceci est un message`}/>
+            <Msg msg={`Ceci est un autre message`}/>
+            <Msg msg={`Ceci est un message trèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèèès long`}/>
+          </div>
         </div>
-        <div className='flex self-center items-center rounded-full text-white bg-stone-800 border border-stone-600 p-2 gap-4 w-1/2 relative'>
-          <input 
-            type="text" 
-            className='flex-1 bg-transparent outline-none text-lg ml-4 placeholder-stone-500' 
+        <div className='flex self-center items-center rounded-3xl text-white bg-stone-800 border border-stone-600 p-2 gap-4 w-2/5 relative'>
+          <textarea  
+            className='overflow-y-auto flex-1 bg-transparent outline-none text-lg placeholder-stone-500 resize-none max-h-[10lh] p-5'
             placeholder={`Demander à ${activeModel}...`}
+            onChange={textareaSize}
+            rows={1}
+            value={inputValue}
           />
-          <div className='w-px h-8 bg-stone-600'></div>
+          <div className='w-px h-[90%] bg-stone-600'></div>
           <button 
             className='bg-[#ffffff10] hover:bg-[#ffffff20] text-white rounded-full cursor-pointer px-6 py-2 text-base border border-[#ffffff20] transition-colors whitespace-nowrap' 
-            onClick={Menu}
+            onClick={handleMenu}
           >
             {activeModel} ⯆
           </button>
---
+++ src/components/Msg.tsx
@@ -0,0 +1,9 @@
+function Msg({msg}: {msg: string}) {
+    return (
+        <div className="flex w-max max-w-full h-max bg-[#ffffff10] rounded-tr-xl rounded-br-xl rounded-bl-xl p-5 items-start break-all">
+            <span>{msg}</span>
+        </div>
+    )
+}
+
+export default Msg
