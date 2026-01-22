0b3f571a7147b001db79913cfa29ef8d0aab7548
Edouard Bautz <edouardbautz@gmail.com>
2026-01-22 13:45:59 +0100

    Add Hello World button to App component

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,9 @@ function App() {
           <button className='bg-[#ffffff10] hover:bg-blue-600 hover:border-blue-500 text-white rounded-full flex items-center justify-center border border-[#ffffff20] w-12 h-12 cursor-pointer transition-all'>
             ➤
           </button>
+          <button className='bg-[#ffffff10] hover:bg-blue-600 hover:border-blue-500 text-white rounded-full flex items-center justify-center border border-[#ffffff20] w-12 h-12 cursor-pointer transition-all'>
+            Hello World !
+          </button>
         </div>
       </div>
     </div>
